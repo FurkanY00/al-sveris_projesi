07e338d8b6608c613c1a2e35d043b3ed1a16b675
Furkan YILMAZ <125738284+FurkanY00@users.noreply.github.com>
2023-10-11 15:31:20 +0300

    Add project files.

---
+++ alısveris_projesi/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace alısveris_projesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglnatı = new SqlConnection(@"Data Source=DESKTOP-KPC6PV7\SQLEXPRESS;Initial Catalog=hareket;Integrated Security=True");
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("execute proje6", baglnatı); 
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
